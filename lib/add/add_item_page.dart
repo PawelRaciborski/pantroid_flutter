4574ab7939322a62ee5a95d9c5b7edf540eb62ea
Paweł Raciborski <raciborski.p@gmail.com>
2019-10-29 21:06:32 +0100

    #1: Adding extra date and save button.

--- lib/add/add_item_page.dart
+++ lib/add/add_item_page.dart
@@ -1,7 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'date_picker.dart';
 
 class AddItemPage extends StatelessWidget {
   static const route = "/add";
@@ -16,29 +16,30 @@ class AddItemPage extends StatelessWidget {
         children: <Widget>[
           _buildNameInput(),
           _buildQuantityInput(),
-          _buildDateInput(context)
+          _buildDateInput(context, "Adding date", (dateTime) {}),
+          _buildDateInput(context, "Expiration date", (dateTime) {}),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0),
+            width: double.infinity,
+            child: RaisedButton(
+              child: Text("Save"),
+              onPressed: () {},
+            ),
+          )
         ],
       ),
     );
   }
 
-  Widget _buildDateInput(BuildContext context) {
+  Widget _buildDateInput(
+      BuildContext context, String hint, Function(DateTime) handler) {
     return Container(
-      margin: const EdgeInsets.all(5.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-              flex: 1,
-              child: _DatePicker(onDateSelected: (dateTime) {
-                debugPrint(dateTime.toString());
-              })),
-          IconButton(
-            icon: Icon(Icons.calendar_today),
-            onPressed: () {
-              _showDatePicker(context);
-            },
-          ),
-        ],
+      margin: const EdgeInsets.all(15.0),
+      child: DatePicker(
+        onDateSelected: (dateTime) {
+          debugPrint(dateTime.toString());
+        },
+        hint: hint,
       ),
     );
   }
@@ -74,60 +75,4 @@ class AddItemPage extends StatelessWidget {
           ),
         ),
       );
-
-  Future<void> _showDatePicker(BuildContext context) async {}
-}
-
-class _DatePicker extends StatefulWidget {
-  final DateTime selectedDate;
-  final Function(DateTime) onDateSelected;
-
-  _DatePicker({this.selectedDate, @required this.onDateSelected});
-
-  @override
-  State<StatefulWidget> createState() => _DatePickerState(
-      selectedDate: selectedDate);
-}
-
-class _DatePickerState extends State<_DatePicker> {
-  DateTime selectedDate;
-
-  _DatePickerState({this.selectedDate});
-
-  String get _displayText {
-    if (selectedDate != null) {
-      return selectedDate.toString();
-    } else {
-      return "Adding date";
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Text(
-        _displayText,
-      ),
-      onTap: () => _showDatePicker(context, selectedDate, (dateTime) {
-        setState(() {
-          selectedDate = dateTime;
-        });
-        widget.onDateSelected(dateTime);
-      }),
-    );
-  }
-
-  Future<void> _showDatePicker(
-    BuildContext context,
-    DateTime initialDate,
-    Function(DateTime) onDateSelected,
-  ) async {
-    final DateTime picked = await showDatePicker(
-      context: context,
-      firstDate: DateTime.fromMillisecondsSinceEpoch(0),
-      lastDate: DateTime.now().add(Duration(days: 1000)),
-      initialDate: initialDate ?? DateTime.now(),
-    );
-    onDateSelected(picked);
-  }
 }
