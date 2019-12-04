39bab6a8af0c51235c81e99f959d0c00d443af98
Paweł Raciborski <raciborski.p@gmail.com>
2019-12-04 14:30:42 +0100

    #3: Layout refactoring

--- lib/add/add_item_page.dart
+++ lib/add/add_item_page.dart
@@ -46,67 +46,76 @@ class _AddItemPageState extends State<AddItemPage> {
             }
           },
           child: BlocBuilder<AddItemBloc, AddItemState>(
-            builder: (context, state) {
-              return Column(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(5, 10, 5, 5),
-                    child: TextFormField(
-                      maxLines: 1,
-                      controller: _controller,
-                      maxLength: 200,
-                      autovalidate: true,
-                      validator: (_) => state.isNameValid ? null : "Name invalid",
-                      decoration: InputDecoration(
-                        labelText: 'Item name',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  _buildQuantityInput(),
-                  _buildDateInput(context, "Adding date", (dateTime) {
-                    _addItemBloc
-                        .add(AddItemAddingDateChangedEvent(dateTime: dateTime));
-                  }, () => true),
-                  _buildDateInput(context, "Expiration date", (dateTime) {
-                    _addItemBloc.add(
-                        AddItemExpirationDateChangedEvent(dateTime: dateTime));
-                  }, () => state.isDateValid),
-                  Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10.0),
-                      width: double.infinity,
-                      child: RaisedButton(
-                        child: Text("Save"),
-                        onPressed: state.idFormValid
-                            ? () {
-                                _addItemBloc.add(SubmitAddItemFormEvent());
-                              }
-                            : null,
-                      )),
-                ],
-              );
-            },
+            builder: (context, state) => Column(
+              children: <Widget>[
+                _buildNameInput(state),
+                _buildQuantityInput(),
+                _buildAddingDateInput(context),
+                _buildExpirationDateInput(context, state),
+                _buildSaveButton(state),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  Widget _buildDateInput(BuildContext context, String hint,
-      Function(DateTime) handler, Function validator) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      child: DatePicker(
-        onDateSelected: handler,
-        hint: hint,
-        validator: validator,
-      ),
-    );
-  }
+  Container _buildSaveButton(AddItemState state) => Container(
+      margin: EdgeInsets.symmetric(horizontal: 10.0),
+      width: double.infinity,
+      child: RaisedButton(
+        child: Text("Save"),
+        onPressed: state.idFormValid
+            ? () {
+                _addItemBloc.add(SubmitAddItemFormEvent());
+              }
+            : null,
+      ));
+
+  Container _buildNameInput(AddItemState state) => Container(
+        margin: const EdgeInsets.fromLTRB(5, 10, 5, 5),
+        child: TextFormField(
+          maxLines: 1,
+          controller: _controller,
+          maxLength: 200,
+          autovalidate: true,
+          validator: (_) => state.isNameValid ? null : "Name invalid",
+          decoration: InputDecoration(
+            labelText: 'Item name',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+            ),
+          ),
+        ),
+      );
+
+  Widget _buildAddingDateInput(BuildContext context) => Container(
+        margin: const EdgeInsets.all(15.0),
+        child: DatePicker(
+          selectedDate: DateTime.now(),
+          onDateSelected: (dateTime) {
+            _addItemBloc.add(AddItemAddingDateChangedEvent(dateTime: dateTime));
+          },
+          hint: "Adding date",
+          validator: () => true,
+        ),
+      );
+
+  Widget _buildExpirationDateInput(BuildContext context, AddItemState state) =>
+      Container(
+        margin: const EdgeInsets.all(15.0),
+        child: DatePicker(
+          onDateSelected: (dateTime) {
+            _addItemBloc
+                .add(AddItemExpirationDateChangedEvent(dateTime: dateTime));
+          },
+          hint: "Expiration date",
+          validator: () => state.isDateValid,
+        ),
+      );
 
   Widget _buildQuantityInput() => Container(
         margin: const EdgeInsets.all(5.0),
